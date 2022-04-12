ef0f116e7f21cb75b161b35ff356f8d4b05ca432
Panda <PandaCatPlayz@gmail.com>
2022-04-12 11:13:42 -0700

    Create complementaryLoops.c

---
+++ complementaryLoops.c
@@ -0,0 +1,19 @@
+int i = 0;
+
+void setup(){
+    // Normal Setup Code
+
+    // Complementary Loop
+    complementaryLoop();
+}
+
+void loop(){
+    // Normal Loop Code
+}
+
+void complementaryLoop(){
+    // Launch more loops here
+    while(i == 0){
+        // Secondary Loop Code
+    }
+}
